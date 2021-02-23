ce05a952511cb14277d7abb03cf7d014c3ea6057
Viragh Krisztian <viragh.krisztian@pataky.hu>
2021-02-23 07:33:33 +0100

    radio button

--- Urlap/js/myscript.js
+++ Urlap/js/myscript.js
@@ -2,6 +2,20 @@ function form() {
     var mail = document.getElementById('mail').value;
     var pass1 = document.getElementById('jelsz1').value;
     var pass2 = document.getElementById('jelsz2').value;
-    var gen = document.getElementById('gender').value;
+    var gt=("male","female","csiga");
+    var gen ="";
+    var radio = document.querySelectorAll("input[name='gender']");
+
+    for( var i=0;i<radio.length;i++){
+        
+        var x = radio[i].checked;
+        if (x){
+            gen = radio[i].value;
+            break;
+        }
+    }
+
+
+    
     document.getElementById('er').innerHTML=mail+" "+pass1+" "+pass2+" "+gen;
 }
