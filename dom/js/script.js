5d3381d93e61b58f3c6c81ae12bf58024b7ff4ec
Viragh Krisztian <viragh.krisztian@pataky.hu>
2021-03-02 08:17:01 +0100

    dom 2

---
+++ dom/js/script.js
@@ -0,0 +1,13 @@
+function be(){
+   document.getElementById('lampa1').src="pic/pic_bulbon.gif";
+}
+function ki(){
+    document.getElementById('lampa1').src="pic/pic_bulboff.gif";
+ }
+
+ function obe(){
+    document.getElementById('lampa2').src="pic/pic_bulbon.gif";
+ }
+ function oki(){
+     document.getElementById('lampa2').src="pic/pic_bulboff.gif";
+  }
