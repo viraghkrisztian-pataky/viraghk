5d8a1353c6f1ea80680cedaec3b40beddde77e8d
Viragh Krisztian <viragh.krisztian@pataky.hu>
2021-03-02 08:59:41 +0100

    image map

--- dom/js/script.js
+++ dom/js/script.js
@@ -11,3 +11,18 @@ function ki(){
  function oki(){
      document.getElementById('lampa2').src="pic/pic_bulboff.gif";
   }
+  var izzo = 0;
+  function lampa(){
+      if (izzo == 0){
+        document.getElementById('lampa3').src="pic/pic_bulbon.gif";
+        izzo = 1;
+      }else{
+        document.getElementById('lampa3').src="pic/pic_bulboff.gif";
+        izzo = 0;
+      }
+
+  }
+
+  function miez(szoveg){
+    document.getElementById('er').innerHTML=szoveg;
+  }
