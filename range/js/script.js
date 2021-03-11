906a1ca2a33035b45859406501ae807a75029fbd
Viragh Krisztian <viragh.krisztian@pataky.hu>
2021-03-11 13:57:05 +0100

    gyengenlato beallitas

--- range/js/script.js
+++ range/js/script.js
@@ -3,3 +3,15 @@ function r1(){
     document.getElementById('er').innerHTML=w;
     document.getElementById('kep').width=w;
 }
+function szin(x){
+    document.body.style.background = x;
+    if (x == 'black'){
+        document.body.style.color = "orange";
+        document.body.style.fontSize = "200%";
+        document.body.style.fontFamily = "Arial, Helvetica, sans-serif";
+    }else{
+        document.body.style.color = "black";
+        document.body.style.fontSize = "100%";
+        document.body.style.fontFamily = "'Times New Roman', Times, serif";
+    }
+}
