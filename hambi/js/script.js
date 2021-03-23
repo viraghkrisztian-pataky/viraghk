2a5b1a9cc8df60bdbfb0231de411c22dff8b039c
Viragh Krisztian <viragh.krisztian@pataky.hu>
2021-03-23 08:56:58 +0100

    1 0 csere

--- hambi/js/script.js
+++ hambi/js/script.js
@@ -1,27 +1,27 @@
-var kepcsere = [0, 0, 0, 0, 0, 0, 0];
+var kepcsere = [1, 1, 1, 1, 1, 1, 1];
 var kepszama=0;
 function csere(kep){
     if(kep == "h1" ||  kep == "h7") {
-        if (kepcsere[0]==0){
+        if (kepcsere[0]==1){
             document.getElementById('h1').src="pic/h1f.png";
             document.getElementById('h7').src="pic/h7f.png";
-            kepcsere[0]=1;
-            kepcsere[6]=1;
+            kepcsere[0]=0;
+            kepcsere[6]=0;
          }else{
             document.getElementById('h1').src="pic/h1.png";
             document.getElementById('h7').src="pic/h7.png";
-             kepcsere[0]=0;
-            kepcsere[6]=0;
+             kepcsere[0]=1;
+            kepcsere[6]=1;
          }
         }else{ 
                     // document.getElementById('teszt').innerHTML=kep;
                 kepszama=kep.slice(1,2); //->1
-                if (kepcsere[kepszama-1]==0){
+                if (kepcsere[kepszama-1]==1){
                     document.getElementById(kep).src="pic/"+kep+"f.png";
-                    kepcsere[kepszama-1]=1;
+                    kepcsere[kepszama-1]=0;
                     }else{
                         document.getElementById(kep).src="pic/"+kep+".png";
-                        kepcsere[kepszama-1]=0;
+                        kepcsere[kepszama-1]=1;
                     }
          }
 
