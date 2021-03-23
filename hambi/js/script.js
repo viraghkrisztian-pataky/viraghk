cf4d3f3e195f2ce565669d91ef4fe5a4beca7739
Viragh Krisztian <viragh.krisztian@pataky.hu>
2021-03-23 08:50:52 +0100

    pufik egyszerre

--- hambi/js/script.js
+++ hambi/js/script.js
@@ -1,14 +1,30 @@
 var kepcsere = [0, 0, 0, 0, 0, 0, 0];
 var kepszama=0;
 function csere(kep){
-   // document.getElementById('teszt').innerHTML=kep;
-   kepszama=kep.slice(1,2); //->1
-   if (kepcsere[kepszama-1]==0){
-       document.getElementById(kep).src="pic/"+kep+"f.png";
-       kepcsere[kepszama-1]=1;
-    }else{
-        document.getElementById(kep).src="pic/"+kep+".png";
-        kepcsere[kepszama-1]=0;
-    }
+    if(kep == "h1" ||  kep == "h7") {
+        if (kepcsere[0]==0){
+            document.getElementById('h1').src="pic/h1f.png";
+            document.getElementById('h7').src="pic/h7f.png";
+            kepcsere[0]=1;
+            kepcsere[6]=1;
+         }else{
+            document.getElementById('h1').src="pic/h1.png";
+            document.getElementById('h7').src="pic/h7.png";
+             kepcsere[0]=0;
+            kepcsere[6]=0;
+         }
+        }else{ 
+                    // document.getElementById('teszt').innerHTML=kep;
+                kepszama=kep.slice(1,2); //->1
+                if (kepcsere[kepszama-1]==0){
+                    document.getElementById(kep).src="pic/"+kep+"f.png";
+                    kepcsere[kepszama-1]=1;
+                    }else{
+                        document.getElementById(kep).src="pic/"+kep+".png";
+                        kepcsere[kepszama-1]=0;
+                    }
+         }
 
 }
+
+
