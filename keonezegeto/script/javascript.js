343115fa59c3b9a77093963295efef5525cccc29
Viragh Krisztian <viragh.krisztian@pataky.hu>
2021-03-30 09:07:54 +0200

    jól megy

--- keonezegeto/script/javascript.js
+++ keonezegeto/script/javascript.js
@@ -1,6 +1,11 @@
 var i = 0;
+var e = 0;
+var u = 0;
+
 function kepn(){
     var kep = document.getElementById('pic');
+    var kepe = document.getElementById('pice');
+    var kepu = document.getElementById('picu');
     if(i<1){
         i=4;
     }
@@ -8,12 +13,34 @@ function kepn(){
         i=1;
     }
     kep.src="pic/img_"+i+".jpg";
+
+    e=i-1;
+    u=i+1;
+
+    if(e<1){
+        e=4;
+    }
+    if(u<1){
+        u=4;
+    }
+    if(e>4){
+        e=1;
+    }
+    if(u>4){
+        u=1;
+    }
+
+    kepe.src="pic/img_"+e+".jpg";
+    kepu.src="pic/img_"+u+".jpg";
+    
 }
 function bal(){
     i--;
+
     kepn();
 }
 function jobb(){
     i++;
+
     kepn();
 }
