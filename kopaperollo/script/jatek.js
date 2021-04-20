91dc85b6e97d7b449317f8dea848547ed6214006
Viragh Krisztian <viragh.krisztian@pataky.hu>
2021-04-20 08:03:20 +0200

    switch case

--- kopaperollo/script/jatek.js
+++ kopaperollo/script/jatek.js
@@ -2,10 +2,30 @@ function jatek(){
     tomb=["ko","papir","ollo"];
     szam=Math.floor(Math.random()*10)%3;
     document.getElementById('egyben').src="pic/"+tomb[szam]+".png";
+    var vendeg="";
+    var robot="";
+    if(document.getElementById('ko').checked){
+        vendeg=document.getElementById('ko').value;
+    }
+    if(document.getElementById('papir').checked){
+        vendeg=document.getElementById('papir').value;
+    }
+    if(document.getElementById('ollo').checked){
+        vendeg=document.getElementById('ollo').value;
+    }
 
-
-
-
-
-    document.getElementById('er').innerHTML=tomb[szam];
+    robot=tomb[szam];
+    switch (x) {
+        case "ko":
+          text = "kő";
+          break;
+        case "papir":
+          text = "papír";
+          break;
+        case "ollo":
+          text = "olló";
+          break;
+        default:
+          text = "No value found";
+    document.getElementById('er').innerHTML=vendeg+"+"+robot;
 }
