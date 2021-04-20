a9691df68aee4c408444eed8301b1a522cfa50df
Viragh Krisztian <viragh.krisztian@pataky.hu>
2021-04-20 09:00:01 +0200

    kész

--- kopaperollo/script/jatek.js
+++ kopaperollo/script/jatek.js
@@ -1,9 +1,13 @@
+var rer=0;
+var ver=0;
+gomb.disabled=false;
 function jatek(){
     tomb=["ko","papir","ollo"];
     szam=Math.floor(Math.random()*10)%3;
     document.getElementById('egyben').src="pic/"+tomb[szam]+".png";
     var vendeg="";
     var robot="";
+
     if(document.getElementById('ko').checked){
         vendeg=document.getElementById('ko').value;
     }
@@ -22,27 +26,48 @@ function jatek(){
             case "ko":
                 if(robot=="papir"){
                     document.getElementById('er').innerHTML="Robot nyert!";
+                    rer++;
                 }else{
                     document.getElementById('er').innerHTML="Vendég nyert!";
+                    ver++;
                 }
               break;
             case "papir":
                 if(robot=="ollo"){
                     document.getElementById('er').innerHTML="Robot nyert!";
+                    rer++;
                 }else{
                     document.getElementById('er').innerHTML="Vendég nyert!";
+                    ver++;
                 }
               break;
             case "ollo":
                 if(robot=="ko"){
                     document.getElementById('er').innerHTML="Robot nyert!";
+                    rer++;
                 }else{
                     document.getElementById('er').innerHTML="Vendég nyert!";
+                    ver++;
                 }
               break;
             }
     }
     
-          
+    document.getElementById('rer').innerHTML=rer;   
+    document.getElementById('ver').innerHTML=ver;  
+    var nyertes="";
+    if(ver>rer) {
+        nyertes="Vendég";
+    }else{
+        nyertes="Robot";
+    }
+    if(ver+rer<3) {
+       gomb.disabled=false;
+    } else{
+        gomb.disabled=true;
+        gomb.style.backgroundColor="grey";
+        alert("A "+nyertes+" nyert!");
+        
+    }
     document.getElementById('teszt').innerHTML=vendeg+"+"+robot;
 }
